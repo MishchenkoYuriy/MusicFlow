f7ce59c2f4c89efa9ba112b626ba4199fb4b123c
Yuriy Mishchenko <y1mishchenko@gmail.com>
2023-08-15 19:38:46 +0600

    Add my playlists ETL from BigQuery to Spotify

---
+++ dags/spotify_etl.py
@@ -0,0 +1,180 @@
+import os
+import math
+import pandas as pd
+from dotenv import load_dotenv
+import logging
+from youtube_etl import load_to_bigquery
+
+from google.cloud import bigquery
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# from datetime import datetime
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
+# from airflow.models import Variable
+
+
+# default_args = {
+#     'owner': 'spotify_client'
+# }
+
+load_dotenv()
+
+
+def extract_df_playlists() -> pd.DataFrame:
+    project_id = os.getenv('PROJECT_ID')
+    client = bigquery.Client(project=project_id)
+
+    sql = f"""
+    SELECT youtube_playlist_id, playlist_name
+    FROM `{project_id}.marts.youtube_playlists`
+    ORDER BY playlist_name
+    LIMIT 1
+    """
+
+    df_playlists = client.query(sql).to_dataframe()
+    return df_playlists
+
+
+def extract_df_videos() -> pd.DataFrame:
+    project_id = os.getenv('PROJECT_ID')
+    client = bigquery.Client(project=project_id)
+
+    sql = f"""
+    SELECT p.playlist_name, v.channel_name, v.title, v.duration_ms
+    FROM `{project_id}.marts.youtube_videos` v
+    INNER JOIN `{project_id}.marts.youtube_playlists` p
+    ON v.youtube_playlist_id = p.youtube_playlist_id
+    ORDER BY p.playlist_name, v.channel_name, v.title, v.duration_ms
+    """
+
+    df_videos = client.query(sql).to_dataframe()
+    return df_videos
+
+
+def get_authorization_code():
+    scope = ["user-library-modify", "playlist-modify-private"]
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    return sp
+
+
+def get_current_user_id() -> str:
+    user_info = sp.current_user()
+    return user_info['id']
+
+
+def create_playlist_from_df_row(row):
+    playlist_info = sp.user_playlist_create(user_id, row['playlist_name'], public=False, collaborative=False)
+    return playlist_info['id']
+
+
+def create_spotify_playlists_from_df(df_playlists: pd.DataFrame) -> pd.DataFrame:
+    """
+    Create private, non-collaborative Spotify playlists from the dataframe.
+    Save created playlist ids as a column in the original dataframe.
+    """
+    df_playlists['spotify_playlist_id'] = df_playlists.apply(create_playlist_from_df_row, axis = 1)
+    return df_playlists
+
+
+def get_spotify_playlist_id(row) -> str:
+    playlist = df_playlists[df_playlists['playlist_name'] == row['playlist_name']]
+    if playlist.empty:
+        logging.info(f'Spotify id not found for playlist "{row["playlist_name"]}", video "{row["title"]}" skipped.')
+        return
+    elif len(playlist) > 1:
+        logging.info(f'{len(playlist)} spotify ids were found for playlist: "{row["playlist_name"]}", first id was chosen.')
+    return playlist.iloc[0, 2]
+
+
+def get_spotify_track_uri(row) -> str:
+    min_diff2 = math.inf
+    track_uri, min_diff = search_spotify_track(row['title'], row['duration_ms'])
+
+    if min_diff > 5000: # abs grater than 5 seconds
+        print(f'WARNING: Track {row["title"]} not found by title, tring to add channel_name to search')
+        track_uri2, min_diff2 = search_spotify_track(f'{row["channel_name"]} {row["title"]}', row['duration_ms'])
+    
+    return track_uri if min_diff < min_diff2 else track_uri2
+
+
+def search_spotify_track(q: str, track_duration_ms: int) -> tuple[str, int]:
+    results = sp.search(q=q, limit=15, type='track')
+    tracks = results['tracks']['items']
+
+    min_diff_track_num = 0
+    min_diff = math.inf
+
+    for track_num, track in enumerate(tracks):
+        diff = abs(track['duration_ms'] - track_duration_ms)
+
+        if diff <= 5000: # difference in 5 seconds is fine
+            print(f'Track found on try: {track_num}')
+            return track['uri'], diff
+        
+        if diff < min_diff:
+            min_diff = diff
+            min_diff_track_num = track_num
+    
+    return tracks[min_diff_track_num]['uri'], min_diff
+
+
+def add_videos_to_playlists(row) -> None:
+    spotify_playlist_id = get_spotify_playlist_id(row)
+
+    # TODO: YouTube video is probably a album, priority: album -> playlist
+    
+    # YouTube video is probably a track
+    track_uri = get_spotify_track_uri(row)
+    print(spotify_playlist_id, track_uri)
+    sp.playlist_add_items(spotify_playlist_id, [track_uri])
+
+
+# def add_to_saved_tracks(track_id: str) -> None:
+#     sp.current_user_saved_tracks_add(tracks=[track_id])
+
+
+# def load_to_spotify(row) -> None:
+#     item_id = find_spotify_id(row)
+#     add_to_saved_tracks(item_id)
+
+
+if __name__ == '__main__':
+    # extract dataframes from BigQuery
+    df_playlists = extract_df_playlists()
+    df_videos = extract_df_videos()
+    
+    # authorisation
+    sp = get_authorization_code()
+    user_id = get_current_user_id()
+
+    # create and store playlists info
+    df_playlists = create_spotify_playlists_from_df(df_playlists)
+    load_to_bigquery(df_playlists[['spotify_playlist_id', 'playlist_name']], 'spotify_playlists')
+    load_to_bigquery(df_playlists[['youtube_playlist_id', 'spotify_playlist_id']], 'playlists_ids')
+
+    df_test = df_videos[df_videos['playlist_name'] == os.getenv('PLAYLIST_NAME')]
+    df_test.apply(add_videos_to_playlists, axis = 1)
+
+    #df.apply(load_to_spotify, axis = 1)
+
+
+
+# with DAG(
+#     dag_id='request_access_token',
+#     default_args=default_args,
+#     description='Spotify DAG',
+#     start_date=datetime(2023, 8, 11),
+#     schedule='@hourly', # None
+#     catchup=False,
+# ) as dag:
+    
+#     request_access_token_task = PythonOperator(
+#         task_id = 'request_access_token_task',
+#         python_callable=request_access_token
+#     )
+    
+    
+#     request_access_token_task
--- dags/youtube_etl.py
+++ dags/youtube_etl.py
@@ -5,6 +5,7 @@ Google OAuth 2.0      OAuth 2.0 provides authenticated access to an API.
 
 import os
 import pickle
+import aniso8601
 import pandas as pd
 import logging
 from dotenv import load_dotenv
@@ -27,7 +28,8 @@ default_args = {
 
 load_dotenv()
 
-videos = {}
+videos: dict[str, list[str]] = {}
+
 
 def get_new_credentials():
     client_secrets_path = os.getenv('CLIENT_SECRETS_PATH')
@@ -61,8 +63,11 @@ def get_valid_credentials():
     return credentials
 
 
-def extract_my_playlists(youtube) -> dict:
-    
+def extract_my_playlists(youtube) -> dict[str, str]:
+    """
+    Return a dictionary object that contains playlist ids and titles
+    of all music playlists created by current user.
+    """
     request = youtube.playlists().list(
         part="snippet,contentDetails",
         maxResults=50,
@@ -70,7 +75,7 @@ def extract_my_playlists(youtube) -> dict:
     )
     response = request.execute()
 
-    playlists = {}
+    playlists: dict[str, str] = {}
     
     for item in response['items']:
         if '💼' not in item['snippet']['title']: # remove non-music playlists
@@ -81,6 +86,10 @@ def extract_my_playlists(youtube) -> dict:
 
 
 def extract_playlist_videos(youtube, playlist_id: str, nextPageToken: str = ''):
+    """
+    Return a requested page of the API call as a json object.
+    Contains information about videos in the playlist.
+    """
     if nextPageToken:
         request = youtube.playlistItems().list(
                 part="snippet,contentDetails",
@@ -99,43 +108,92 @@ def extract_playlist_videos(youtube, playlist_id: str, nextPageToken: str = ''):
     return response
 
 
-def populate_videos(response):
+def populate_videos(response, playlist_id: str) -> None:
     for item in response['items']:
         # Remove deleted, private and duplicate videos
         if item['snippet']['title'] not in ('Deleted video', 'Private video') and item['contentDetails']['videoId'] not in videos:
-            videos[item['contentDetails']['videoId']] = (item['snippet']['title'], item['snippet']['videoOwnerChannelTitle'])
+            videos[item['contentDetails']['videoId']] = [playlist_id, item['snippet']['title'], item['snippet']['videoOwnerChannelTitle']]
 
 
-def extract_all_videos(youtube, playlists: dict):
+def extract_all_videos(youtube, playlists: dict[str, str]) -> None:
+    """
+    Extract playlist items info (video ids, titles and channel names)
+    and store it in the video dictionary ( dict[str, list[str]] ).
+
+    Args:
+        playlists: iterable object that contains playlist ids
+    """
     for playlist_id in playlists:
     
         response = extract_playlist_videos(youtube, playlist_id)
-        populate_videos(response)
+        populate_videos(response, playlist_id)
 
         while response.get('nextPageToken', ''):
             response = extract_playlist_videos(youtube, playlist_id, response['nextPageToken'])
-            populate_videos(response)
+            populate_videos(response, playlist_id)
+
+
+def add_ms_duration(youtube) -> None:
+    """
+    Split video ids into 50-size chunks, call videos().list for each chunk.
+    Extract video duration, convert it to milliseconds and store it in the video dictionary ( dict[str, list[str]] ).
+    """
+    chunks: list[list[str]] = []
+    chunk: list[str] = []
+
+    for ind, video in enumerate(videos):
+        if ind % 50 == 0 and chunk: # not include the first empty chunk
+            chunks.append(chunk)
+            logging.info(f'Chunk with the length of {len(chunk)} was added, total chunks added: {len(chunks)}')
+            chunk = []
+        chunk.append(video)
+
+    chunks.append(chunk) # append the last chunk
+    logging.info(f'Chunk with the length of {len(chunk)} was added, total chunks added: {len(chunks)}')
+    
+    str_chunks: list[str] = [','.join(chunk) for chunk in chunks]
+
+    for chunk_ind, str_chunk in enumerate(str_chunks):
+        request = youtube.videos().list(part="snippet,contentDetails", id=str_chunk)
+        response = request.execute()
+        
+        for item_ind, item in enumerate(response['items']):
+            video_id = chunks[chunk_ind][item_ind] # get the videoId
+            
+            iso8601_duration = item['contentDetails']['duration']
+            duration_ms = int(aniso8601.parse_duration(iso8601_duration).total_seconds()*1000)
+            videos[video_id].append(duration_ms)
 
 
 def transform_playlists_to_df(playlists: dict) -> pd.DataFrame:
+    """
+    Return a playlists dataframe from a playlist dict[str, str] object.
+    """
     playlists_series = pd.Series(playlists)
-    df_playlists = pd.DataFrame(playlists_series, columns=['playlist_name'])
+    df_playlists = pd.DataFrame(playlists_series, columns=['playlist_name']).reset_index(names='youtube_playlist_id')
     return df_playlists
 
 
-def transform_videos_to_df(videos: dict) -> pd.DataFrame:
-    df_videos = pd.DataFrame(videos).T
-    df_videos = df_videos.rename(columns={0: 'title', 1: 'channel_name'})
+def transform_videos_to_df() -> pd.DataFrame:
+    """
+    Return a videos dataframe from a videos dict[str, list[str]] object.
+    """
+    df_videos = pd.DataFrame.from_dict(videos, orient='index',
+                                       columns=['youtube_playlist_id', 'title', 'channel_name', 'duration_ms']).reset_index(names='video_id')
     return df_videos
 
 
 def load_to_bigquery(df: pd.DataFrame, table_name: str) -> None:
+    """
+    Create or replace a table in the marts dataset in Google BigQuery
+    and populate it with data from the pandas dataframe.
+    """
     project_id = os.getenv('PROJECT_ID')
     client = bigquery.Client(project=project_id)
+    job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE") # replace the table
     table_id = f'{project_id}.marts.{table_name}'
 
-    # Create a table in the marts dataset and populate it with data from the dataframe
-    client.load_table_from_dataframe(df, table_id).result()
+    client.load_table_from_dataframe(df, table_id, job_config=job_config).result()
     logging.info(f"Dataframe was succecfully loaded to a BigQuery table: {table_name}")
 
 
@@ -145,11 +203,12 @@ if __name__ == '__main__':
 
     playlists = extract_my_playlists(youtube)
     df_playlists = transform_playlists_to_df(playlists)
-    load_to_bigquery(df_playlists, 'playlists')
+    load_to_bigquery(df_playlists, 'youtube_playlists')
 
     extract_all_videos(youtube, playlists)
-    df_videos = transform_videos_to_df(videos)
-    load_to_bigquery(df_videos, 'videos')
+    add_ms_duration(youtube)
+    df_videos = transform_videos_to_df()
+    load_to_bigquery(df_videos, 'youtube_videos')
 
 
 
