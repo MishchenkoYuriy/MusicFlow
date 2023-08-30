fba141259360afc0b3583e21f39e5c1825b74451
y1mishchenko@gmail.com <y1mishchenko@gmail.com>
2023-08-30 03:57:21 +0000

    Add duration_match test

---
+++ dbt/macros/tests/test_duration_match.sql
@@ -0,0 +1,35 @@
+{% test duration_match(model, column_name) %}
+
+with
+
+join_and_calculate as (
+
+    select
+        m.{{ column_name }} as uri,
+        m.duration_ms as model_duration,
+        sum(st.duration_ms) as tracks_duration
+    
+    from {{ model }} m
+
+    inner join {{ ref('stg__spotify_tracks') }} st on m.{{ column_name }} = st.{{ column_name }}
+
+    group by m.{{ column_name }}, model_duration
+
+),
+
+final as (
+
+    select
+        uri,
+        model_duration,
+        tracks_duration
+    
+    from join_and_calculate
+
+    where model_duration != tracks_duration
+
+)
+
+select * from final
+
+{% endtest %}
