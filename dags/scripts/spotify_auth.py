fd92c668f1dbe83379b821ffd036c00883df0625
Yuriy Mishchenko <y1mishchenko@gmail.com>
2023-09-06 17:50:39 +0600

    Move get_refresh_token from setup_dag

--- dags/scripts/spotify_auth.py
+++ dags/scripts/spotify_auth.py
@@ -19,6 +19,34 @@ def print_auth_url(scope: str = 'user-library-modify,playlist-modify-private') -
     print(url)
 
 
+def get_refresh_token(AUTH_CODE):
+        '''
+        Return REFRESH_TOKEN from AUTH_CODE and Spotify credentials.
+        '''
+        client_id = os.getenv('SPOTIPY_CLIENT_ID')
+        client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
+        redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
+
+        credentials = f'{client_id}:{client_secret}'
+        base64_encoded = base64.b64encode(credentials.encode()).decode()
+        response = requests.post(
+            'https://accounts.spotify.com/api/token',
+            data={"grant_type": "authorization_code", "code": AUTH_CODE, "redirect_uri": redirect_uri},
+            headers={"Authorization": "Basic " + base64_encoded},
+        )
+
+        response_json = response.json()
+
+        if not response_json.get("refresh_token"):
+            raise ValueError('REFRESH_TOKEN is null, try to set the AUTH_CODE again.')
+
+        else:
+            # print(f'ACCESS_TOKEN={response_json.get("access_token")}')
+            print(f'REFRESH_TOKEN={response_json.get("refresh_token")}')
+            
+            return response_json.get("refresh_token")
+
+
 def auth_with_client_credentials():
     '''
     The Client Credentials flow is used in server-to-server authentication.
@@ -67,3 +95,4 @@ def auth_with_refresh_token(refresh_token):
 
 if __name__ == "__main__":
     print_auth_url()
+    # get_refresh_token(os.getenv('AUTH_CODE'))
--- dags/setup_dag.py
+++ dags/setup_dag.py
@@ -1,11 +1,10 @@
-import os
-import base64
-import requests
 import logging
 
 from airflow.decorators import dag, task
 from airflow.models import Variable
 from airflow.utils.dates import days_ago
+
+from scripts.spotify_auth import get_refresh_token
 
 
 @dag(
@@ -15,35 +14,15 @@ from airflow.utils.dates import days_ago
 )
 def setup_dag():
     @task
-    def set_refresh_token() -> None:
+    def set_refresh_token():
         '''
         Set or update REFRESH_TOKEN in the Airflow Variables.
         Require AUTH_CODE to be set in the Airflow Variables.
         '''
-        client_id = os.getenv('SPOTIPY_CLIENT_ID')
-        client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
-        redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
         auth_code = Variable.get('AUTH_CODE')
-
-        credentials = f'{client_id}:{client_secret}'
-        base64_encoded = base64.b64encode(credentials.encode()).decode()
-        response = requests.post(
-            'https://accounts.spotify.com/api/token',
-            data={"grant_type": "authorization_code", "code": auth_code, "redirect_uri": redirect_uri},
-            headers={"Authorization": "Basic " + base64_encoded},
-        )
-
-        response_json = response.json()
-
-        if not response_json.get("refresh_token"):
-            raise ValueError('REFRESH_TOKEN is null, try to set the AUTH_CODE again.')
-
-        else:
-            # logging.info(f'ACCESS_TOKEN={response_json.get("access_token")}')
-            logging.info(f'REFRESH_TOKEN={response_json.get("refresh_token")}')
-
-            Variable.set('REFRESH_TOKEN', response_json.get('refresh_token'))
-            logging.info(f'REFRESH_TOKEN has been set in the Airflow Variables.')
+        refresh_token = get_refresh_token(auth_code)
+        Variable.set('REFRESH_TOKEN', refresh_token)
+        logging.info(f'REFRESH_TOKEN has been set in the Airflow Variables.')
 
 
     set_refresh_token()
