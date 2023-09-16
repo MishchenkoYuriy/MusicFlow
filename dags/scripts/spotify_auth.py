867709663ae9bac1de06e71a1e7e820ff306a169
Yuriy Mishchenko <y1mishchenko@gmail.com>
2023-09-16 19:52:49 +0600

    Fix formatting in spotify_auth.py

--- dags/scripts/spotify_auth.py
+++ dags/scripts/spotify_auth.py
@@ -1,97 +1,115 @@
-import os
 import base64
+import os
+
 import requests
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
 
-def print_auth_url(scope: str = 'user-library-modify,playlist-modify-private') -> None:
-    '''
-    Print authentication URL. Used in the project setup to get AUTH_CODE.
-    '''
-    client_id = os.getenv('SPOTIPY_CLIENT_ID')
-    redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
-    url = f'https://accounts.spotify.com/authorize?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}&scope={scope}'
+def print_auth_url(
+    scope: str = "user-library-modify,playlist-modify-private",
+) -> None:
+    """
+    Print authentication URL.
+    Used in the project setup to get AUTH_CODE.
+    """
+    client_id = os.getenv("SPOTIPY_CLIENT_ID")
+    redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
+    url = f"https://accounts.spotify.com/authorize?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}&scope={scope}"
     print(url)
 
 
 def get_refresh_token(AUTH_CODE):
-        '''
-        Return REFRESH_TOKEN from AUTH_CODE and Spotify credentials.
-        '''
-        client_id = os.getenv('SPOTIPY_CLIENT_ID')
-        client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
-        redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
+    """
+    Return REFRESH_TOKEN from AUTH_CODE and Spotify credentials.
+    """
+    client_id = os.getenv("SPOTIPY_CLIENT_ID")
+    client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
+    redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
 
-        credentials = f'{client_id}:{client_secret}'
-        base64_encoded = base64.b64encode(credentials.encode()).decode()
-        response = requests.post(
-            'https://accounts.spotify.com/api/token',
-            data={"grant_type": "authorization_code", "code": AUTH_CODE, "redirect_uri": redirect_uri},
-            headers={"Authorization": "Basic " + base64_encoded},
-        )
+    credentials = f"{client_id}:{client_secret}"
+    base64_encoded = base64.b64encode(credentials.encode()).decode()
+    response = requests.post(
+        "https://accounts.spotify.com/api/token",
+        data={
+            "grant_type": "authorization_code",
+            "code": AUTH_CODE,
+            "redirect_uri": redirect_uri,
+        },
+        headers={"Authorization": "Basic " + base64_encoded},
+    )
 
-        response_json = response.json()
+    response_json = response.json()
 
-        if not response_json.get("refresh_token"):
-            raise ValueError('REFRESH_TOKEN is null, try to set the AUTH_CODE again.')
+    if not response_json.get("refresh_token"):
+        raise ValueError("REFRESH_TOKEN is null, try to set the AUTH_CODE again.")
 
-        else:
-            # print(f'ACCESS_TOKEN={response_json.get("access_token")}')
-            print(f'REFRESH_TOKEN={response_json.get("refresh_token")}')
-            
-            return response_json.get("refresh_token")
+    else:
+        # print(f'ACCESS_TOKEN={response_json.get("access_token")}')
+        print(f'REFRESH_TOKEN={response_json.get("refresh_token")}')
+
+        return response_json.get("refresh_token")
 
 
 def auth_with_client_credentials():
-    '''
-    The Client Credentials flow is used in server-to-server authentication.
-    Without user authentication and access to their information. Not used in this project.
-    '''
-    client_id = os.getenv('SPOTIPY_CLIENT_ID')
-    client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
+    """
+    The Client Credentials flow is used in server-to-server
+    authentication. Without user authentication and access to their
+    information. Not used in this project.
+    """
+    client_id = os.getenv("SPOTIPY_CLIENT_ID")
+    client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
 
-    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+    client_credentials_manager = SpotifyClientCredentials(
+        client_id=client_id, client_secret=client_secret
+    )
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     return sp
 
 
-def auth_with_auth_manager(scope: list = ["user-library-modify", "playlist-modify-private"]):
-    '''
+def auth_with_auth_manager(scope: list):
+    """
     The authorisation code flow includes user authentication.
-    To grant the permissions, the user is redirected to the specified SPOTIPY_REDIRECT_URI
-    the first time and each time the scope is changed. Used for non-Airflow setup.
-    '''
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope), requests_timeout=15, retries=5)
+    To grant the permissions, the user is redirected to the specified
+    SPOTIPY_REDIRECT_URI the first time and each time the scope is
+    changed. Used for non-Airflow setup.
+    """
+    if not scope:
+        scope = ["user-library-modify", "playlist-modify-private"]
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyOAuth(scope=scope), requests_timeout=15, retries=5
+    )
     return sp
 
 
 def auth_with_refresh_token(refresh_token):
-    '''
-    A user authentication that uses the refresh token to generate an access token for a session.
-    Prevent user input altogether. The refresh token must be set as an Airflow Variable.
-    Used for Airflow setup.
-    '''
-    client_id = os.getenv('SPOTIPY_CLIENT_ID')
-    client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
-    
-    credentials = f'{client_id}:{client_secret}'
+    """
+    A user authentication that uses the refresh token to generate
+    an access token for a session. Prevent user input altogether.
+    The refresh token must be set as an Airflow Variable. Used for
+    Airflow setup.
+    """
+    client_id = os.getenv("SPOTIPY_CLIENT_ID")
+    client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
+
+    credentials = f"{client_id}:{client_secret}"
     base64_encoded = base64.b64encode(credentials.encode()).decode()
     response = requests.post(
-        'https://accounts.spotify.com/api/token',
-        data={'grant_type': 'refresh_token', 'refresh_token': refresh_token},
-        headers={'Authorization': 'Basic ' + base64_encoded},
+        "https://accounts.spotify.com/api/token",
+        data={"grant_type": "refresh_token", "refresh_token": refresh_token},
+        headers={"Authorization": "Basic " + base64_encoded},
     )
 
     response_json = response.json()
-    access_token = response_json.get('access_token')
+    access_token = response_json.get("access_token")
     sp = spotipy.Spotify(auth=access_token, requests_timeout=15, retries=5)
     return sp
 
 
 if __name__ == "__main__":
     from dotenv import load_dotenv
+
     load_dotenv()
-    
+
     print_auth_url()
     # get_refresh_token(os.getenv('AUTH_CODE'))
