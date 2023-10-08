ee0e2db868dbc6e35275dd1aa478a5dce5efaa5b
y1mishchenko@gmail.com <y1mishchenko@gmail.com>
2023-10-08 02:04:19 +0000

    Update log_for_tableau

--- dbt/models/marts/log_for_tableau.sql
+++ dbt/models/marts/log_for_tableau.sql
@@ -1,83 +1,120 @@
 {{ config(materialized='view') }}
 
+
 with
 
-join_videos as (
+current_users_music as (
 
     select
-        yl.id,
+        s.log_id,
+        yv.video_id,
         case
-        when yv.duration_ms < {{ env_var('DBT_THRESHOLD_MS') }} then 'track'
-        when yv.duration_ms >= {{ env_var('DBT_THRESHOLD_MS') }} then 'album/playlist'
-        end as video_type
-    
-    from {{ ref('stg__youtube_library') }} yl
-    inner join {{ ref('stg__youtube_videos') }} yv on yl.video_id = yv.video_id
+            when yv.duration_ms < {{ env_var('DBT_THRESHOLD_MS') }} then 'Track'
+            when yv.duration_ms >= {{ env_var('DBT_THRESHOLD_MS') }} then 'Album/Playlist'
+        end as youtube_type,
+        yv.type as music_type,
 
-),
-
-join_spotify_log as (
-
-    select
-        v.id as log_id,
-        v.video_type,
-
-        s.album_uri,
-        s.playlist_uri,
         case
-            when s.album_uri is not null       then 'album'
-            when s.playlist_uri is not null   then 'playlist'
-            when s.track_uri is not null       then 'track'
+            when s.album_uri is not null      then 'Album'
+            when s.playlist_uri is not null   then 'Playlist'
+            when s.track_uri is not null      then 'Track'
         end as spotify_type,
 
         s.found_on_try,
-        s.difference_ms,
-        s.track_match,
         s.search_type_id,
-        s.status
+        s.difference_ms,
+        round(s.difference_ms / 1000, 1) as difference_sec,
+        round(s.difference_ms / 60000, 2) as difference_m,
+        time(timestamp_seconds(div(s.difference_ms, 1000))) as difference_timestamp,
+        s.track_match,
+        s.total_tracks,
+        round((s.track_match / s.total_tracks) * 100, 1) as percentage_in_desc,
+    
+    from {{ ref('stg__youtube_library') }} yl
+    inner join {{ ref('stg__youtube_playlists') }} yp on yl.youtube_playlist_id = yp.youtube_playlist_id
+    inner join {{ ref('stg__youtube_videos') }} yv on yl.video_id = yv.video_id
+    left join {{ ref('stg__spotify_log') }} s on yl.id = s.log_id
 
-    from join_videos v
-    left join {{ ref('stg__spotify_log') }} s on v.id = s.log_id
+    where yp.author = "{{ env_var('DBT_YOUR_CHANNEL_NAME') }}" or yp.author is null
 
 ),
 
-join_total_tracks as (
+other_users_music as (
 
     select
-        sl.log_id,
-        sl.video_type,
-        sl.spotify_type,
-        sl.found_on_try,
-        sl.difference_ms,
-        sl.track_match,
-        sl.search_type_id,
-        sl.status,
-        coalesce(sa.total_tracks, spo.total_tracks, 1) as total_tracks, --TODO
-        round(sl.difference_ms / 1000, 1) as difference_sec
-    
-    from join_spotify_log sl
-    left join {{ ref('stg__spotify_albums')}} sa            on sl.album_uri = sa.album_uri
-    left join {{ ref('stg__spotify_playlists_others')}} spo on sl.playlist_uri = spo.playlist_uri
+        null as log_id,
+        cast(null as string) as video_id,
+        yp.type as youtube_type,
+        cast(null as string) as music_type,
+
+        case
+            when s.album_uri is not null      then 'Album'
+            when s.playlist_uri is not null   then 'Playlist'
+            when s.track_uri is not null      then 'Track'
+        end as spotify_type,
+
+        s.found_on_try,
+        s.search_type_id,
+        s.difference_ms,
+        round(s.difference_ms / 1000, 1) as difference_sec,
+        round(s.difference_ms / 60000, 2) as difference_m,
+        time(timestamp_seconds(div(s.difference_ms, 1000))) as difference_timestamp,
+        s.track_match,
+        s.total_tracks,
+        round((s.track_match / s.total_tracks) * 100, 1) as percentage_in_desc,
+
+    from {{ ref('stg__youtube_library') }} yl
+    inner join {{ ref('stg__youtube_playlists') }} yp on yl.youtube_playlist_id = yp.youtube_playlist_id
+    inner join {{ ref('stg__youtube_videos') }} yv on yl.video_id = yv.video_id
+    left join {{ ref('stg__spotify_log') }} s on yl.id = s.log_id
+
+    where yp.author != "{{ env_var('DBT_YOUR_CHANNEL_NAME') }}" and yp.author is not null
+
+    group by
+        yp.youtube_playlist_id,
+        yp.type,
+        s.album_uri,
+        s.playlist_uri,
+        s.track_uri,
+        s.found_on_try,
+        s.search_type_id,
+        s.difference_ms,
+        s.track_match,
+        s.total_tracks
+
+),
+
+youtube_grouped_library as (
+
+    select * from current_users_music
+    union all
+    select * from other_users_music
 
 ),
 
 final as (
 
     select
+        row_number() over (order by search_type_id) as id,
         log_id,
-        video_type,
+        video_id,
+        youtube_type,
+        music_type,
         spotify_type,
         found_on_try,
+        search_type_id,
         difference_ms,
+        case
+            when difference_sec = 0 then 0.1 --fix for logarithmic scale axis
+            else difference_sec
+        end as difference_sec,
+        difference_m,
+        difference_timestamp,
         track_match,
         total_tracks,
-        search_type_id,
-        status,
-        round((track_match / total_tracks) * 100, 1) as percentage_in_desc,
-        difference_sec
+        percentage_in_desc
     
-    from join_total_tracks
-
+    from youtube_grouped_library
 )
 
 select * from final
